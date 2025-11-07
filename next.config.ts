98a9acdc46fb875d6a0c2ced895db0898d89f1e3
athul <athul.eb21@gmail.com>
2025-11-07 15:49:16 +0530

    implemented posthog

--- next.config.ts
+++ next.config.ts
@@ -11,7 +11,20 @@ const nextConfig: NextConfig = {
   //   unoptimized: false,
   //   domains: []
   // }
-   images: { unoptimized: true },
+  images: { unoptimized: true },
+  async rewrites() {
+    return [
+      {
+        source: "/ingest/static/:path*",
+        destination: "https://us-assets.i.posthog.com/static/:path*",
+      },
+      {
+        source: "/ingest/:path*",
+        destination: "https://us.i.posthog.com/:path*",
+      },
+    ];
+  },
+  skipTrailingSlashRedirect: true,
 };
 
 export default nextConfig;
